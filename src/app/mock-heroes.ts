3b32f5155b36fc7255d74ed4f3af766a5f793025
cmheng <cmheng@outlook.sg>
2022-08-18 17:18:13 +0800

    Display a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Annette' },
+    { id: 13, name: 'Ingrid' },
+    { id: 14, name: 'Mercedes' },
+    { id: 15, name: 'Dorothea' },
+    { id: 16, name: 'Bernadetta' },
+    { id: 17, name: 'Petra' },
+    { id: 18, name: 'Hilda' },
+    { id: 19, name: 'Lysithea' },
+    { id: 20, name: 'Marianne' },
+    { id: 21, name: 'Leonie'}
+];
